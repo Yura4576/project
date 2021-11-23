28403f04d75cf45015574da6fad3e71a9c620a1a
Yura <Yura@gmail.com>
2021-11-23 12:47:46 +0200

    Урок 40. Создание проекта.

--- app/Models/BlogPost.php
+++ app/Models/BlogPost.php
@@ -6,10 +6,37 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
+/**
+ *
+ * @package App\Models
+ *
+ * @property \App\Models\BlogCategory   $category
+ * @property \App\Models\User           $user
+ * @property string                     $title
+ * @property string                     $slug
+ * @property string                     $content_html
+ * @property string                     $content_raw
+ * @property string                     $excerpt
+ * @property string                     $published_at
+ * @property string                     $is_published
+ */
+
 class BlogPost extends Model
 {
     use HasFactory;
     use SoftDeletes;
+
+    protected $fillable
+    = [
+            'title',
+            'slug',
+            'category_id',
+            'excerpt',
+            'content_raw',
+            'is_published_at',
+            'published_at',
+            'user_id',
+        ];
 
     /**
      * Категории статьи.
